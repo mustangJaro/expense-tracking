6e8c630f456bf74689ac5d5b849f831e62dbf082
mustangJaro <ajarosinski@gmail.com>
2023-01-21 05:28:08 -0600

    Initial expense setup

---
+++ src/components/Expenses.js
@@ -0,0 +1,20 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from './Card';
+import "./Expenses.css";
+
+function Expenses(props) {
+
+  return (
+    <Card className='expenses'>
+      {props.items.map((item) => (
+        <ExpenseItem
+          title={item.title}
+          amount={item.amount}
+          date={item.date}
+        ></ExpenseItem>
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
